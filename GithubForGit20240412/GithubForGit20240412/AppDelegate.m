854df7cc1296fafeb6c28a82421dfb48fdb6e916
yellowwing <923133828@qq.com>
2024-04-14 20:08:44 +0800

    经理

--- GithubForGit20240412/GithubForGit20240412/AppDelegate.m
+++ GithubForGit20240412/GithubForGit20240412/AppDelegate.m
@@ -17,14 +17,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    NSLog(@"张三");
-    NSLog(@"张三");
-    NSLog(@"张三");
-    NSLog(@"张三");
-    NSLog(@"张三");
-    NSLog(@"张三");
     
-    
+    NSLog(@"经理");
     NSLog(@"经理");
     
     return YES;
